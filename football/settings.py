4ae9b2fe3bdad66bdc8d9b49e9f8a7569167dcbe
name <email>
2015-12-10 09:33:08 +0200

    Change static root path in settings for ansible

--- football/settings.py
+++ football/settings.py
@@ -114,7 +114,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-STATIC_ROOT = os.path.expanduser('~/myproject/static_root')
+# STATIC_ROOT=os.path.expanduser('~/myproject/static_root')
+STATIC_ROOT = os.path.expanduser('/home/vagrant/project/static_root')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
